9e26afdef8423b7f5b9e17dcdfdb90ef5e254d55
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-05-31 15:59:12 +0200

    aggiunto titolo prova vue

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TitoloProva/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TitoloProva from './components/TitoloProva.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    TitoloProva
+}
 }
 </script>
 
---
+++ src/components/TitoloProva.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>ciao vue e' una bomba</h1>
+</template>
+
+<script>
+export default {
+    name:"TitoloProva"
+}
+</script>
+
+<style>
+    h1{
+        color:gold;
+    }
+</style>
